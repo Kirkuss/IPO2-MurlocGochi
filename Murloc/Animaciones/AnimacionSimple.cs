2f4d6a31c82ea08fdda34d97468f0abff83236b5
Kirkuss <43791767+Kirkuss@users.noreply.github.com>
2018-12-01 16:11:24 +0100

    Add files via upload

---
+++ Murloc/Animaciones/AnimacionSimple.cs
@@ -0,0 +1,35 @@
+﻿using Murloc_Tamagochi.Source.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Animation;
+
+namespace Murloc_Tamagochi.Animaciones
+{
+    class AnimacionSimple
+    {
+        private Storyboard animacion;
+        private Boolean reproduciendo = false;
+
+        public AnimacionSimple(Storyboard animacion)
+        {
+            this.Animacion = animacion;
+        }
+
+        public bool Reproduciendo { get => reproduciendo; set => reproduciendo = value; }
+        public Storyboard Animacion { get => animacion; set => animacion = value; }
+
+        public void Start()
+        {
+            reproduciendo = true;
+            Animacion.Begin();
+        }
+        public void Stop()
+        {
+            reproduciendo = false;
+            Animacion.Stop();
+        }
+    }
+}
