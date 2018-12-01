3ca7df420b1b7ada61fac61a6854849372aefdd9
Kirkuss <43791767+Kirkuss@users.noreply.github.com>
2018-12-01 18:23:46 +0100

    Add files via upload

---
+++ Murloc/Controles/Logro.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Murloc_Tamagochi.Controles
+{
+    /// <summary>
+    /// Lógica de interacción para Logro.xaml
+    /// </summary>
+    public partial class Logro : UserControl
+    {
+        public Logro()
+        {
+            InitializeComponent();
+            
+        }
+    }
+}
